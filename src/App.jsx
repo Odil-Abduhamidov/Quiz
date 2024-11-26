131b848f2eab2591ab00bb50a1d7217c306cdb43
Odil-Abduhamidov <odylabduhamidov@gmail.com>
2024-11-26 19:32:46 +0500

    changedApp.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,22 @@ import "./App.css";
 import "./styles/main.css";
 
 export const App = () => {
-  const [counter, setCounter] = useState(0)
+  // "const [counter, setCounter] = useState(0)
 
-  console.log(counter)
+  // console.log(counter)
 
 
-  const incrementHandler = () => {
-    setCounter(counter + 1)
-  }
+  // const incrementHandler = () => {
+  //   setCounter(counter + 1)
+  // }
 
-  const decrementHandler = () => {
-    setCounter(counter - 1)
-  }
+  // const decrementHandler = () => {
+  //   setCounter(counter - 1)
+  // }"
 
 
   return (
-    <div className="App">
-      <Welcome />
-      <div>
-        <div>{counter}</div>
-        <div style={{ display: 'flex' }}>
-          <button onClick={incrementHandler}>+</button>
-          <button onClick={decrementHandler}>-</button>
-        </div>
-      </div>
-
-    </div>
+    <>
+    </>
   );
 };
